b6cfd891137274b05bc9f91c9d2d9b41c7bcd3c1
Tiago Jorge <t.miguensrijobr@ocado.com>
2020-10-24 22:24:46 +0100

    Create stub input reader namespace

---
+++ include/InputReader.h
@@ -0,0 +1,12 @@
+#ifndef CATCH_GAME_INPUT_READER_H
+#define CATCH_GAME_INPUT_READER_H
+
+#include "Application.h"
+#include "Game.h"
+
+namespace InputReader
+{
+    void ReadInputs(ApplicationInputs &appInputs, GameInputs &gameInputs);
+};
+
+#endif
---
+++ src/InputReader.cpp
@@ -0,0 +1,12 @@
+#include "InputReader.h"
+
+void InputReader::ReadInputs(ApplicationInputs &appInputs, GameInputs &gameInputs)
+{
+    // TODO
+    appInputs.pauseIsPressed = false;
+    appInputs.quitIsPressed = false;
+
+    // TODO
+    gameInputs.moveLeftIsPressed = false;
+    gameInputs.moveRightIsPressed = false;
+}
