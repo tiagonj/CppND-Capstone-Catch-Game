ac306902501e535eb0ce0e771fe048a670d769b9
Tiago Jorge <t.miguensrijobr@ocado.com>
2020-10-24 21:54:34 +0100

    Add Metronome class (regulates game loop duration)

---
+++ include/Metronome.h
@@ -0,0 +1,61 @@
+#ifndef CATCH_GAME_METRONOME_H
+#define CATCH_GAME_METRONOME_H
+
+#include <chrono>
+#include <cstdint>
+#include <deque>
+#include <functional>
+
+using time_value_t = std::chrono::system_clock::time_point;
+
+// Regulates game loop time by throttling the number of updates per second
+// based on the average value of the game loop execution duration
+class Metronome
+{
+  public:
+    Metronome(uint32_t minUpdatesPerSecond, uint32_t maxUpdatesPerSecond);
+    Metronome(uint32_t updatesPerSecond);
+    ~Metronome();
+
+    time_value_t StartMeasurement();
+    void StopMeasurementAndSleepIfApplicable(time_value_t &startTime);
+    uint32_t UpdatesPerSecond();
+
+  private:
+    Metronome(Metronome &) = delete;
+    Metronome(Metronome &&) = delete;
+    Metronome &operator=(Metronome &) = delete;
+    Metronome &operator=(Metronome &&) = delete;
+
+    long TargetDurationInUs();
+    void AdjustInternals(uint32_t executionDurationInUs);
+
+  private:
+    uint32_t _minUpdatesPerSecond{0};
+    uint32_t _maxUpdatesPerSecond{0};
+    uint32_t _updatesPerSecond{0};
+
+    std::deque<uint32_t> _historicExecutionDurationsInUs;
+    uint32_t _executionDurationInUsAccumulator{0};
+};
+
+// Provides clean encapsulation for the Metronome class behaviour
+class AutoMetronome
+{
+  public:
+    AutoMetronome(Metronome &m);
+    ~AutoMetronome();
+
+  private:
+    AutoMetronome() = delete;
+    AutoMetronome(AutoMetronome &) = delete;
+    AutoMetronome(AutoMetronome &&) = delete;
+    AutoMetronome &operator=(AutoMetronome &) = delete;
+    AutoMetronome &operator=(AutoMetronome &&) = delete;
+
+  private:
+    Metronome &_metronome;
+    time_value_t _startTime;
+};
+
+#endif
---
+++ src/Metronome.cpp
@@ -0,0 +1,107 @@
+#include "Metronome.h"
+
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <thread>
+
+#define MICROSECONDS_PER_SECOND ((float)1000000.0f)
+#define MAX_HISTORIC_SAMPLES (100)
+
+Metronome::Metronome(uint32_t minUpdatesPerSecond, uint32_t maxUpdatesPerSecond)
+    : _minUpdatesPerSecond(minUpdatesPerSecond),
+      _maxUpdatesPerSecond(maxUpdatesPerSecond),
+      _updatesPerSecond(minUpdatesPerSecond),
+      _executionDurationInUsAccumulator(0)
+{
+}
+
+Metronome::Metronome(uint32_t updatesPerSecond)
+{
+    Metronome(updatesPerSecond, updatesPerSecond);
+}
+
+Metronome::~Metronome()
+{
+}
+
+time_value_t Metronome::StartMeasurement()
+{
+    return std::chrono::system_clock::now();
+}
+
+void Metronome::StopMeasurementAndSleepIfApplicable(time_value_t &startTime)
+{
+    auto endTime = std::chrono::system_clock::now();
+
+    long executionDurationInUs =
+        std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime).count();
+
+    assert(executionDurationInUs >= 0);
+
+    long targetDurationInUs = TargetDurationInUs();
+
+    if (executionDurationInUs < targetDurationInUs)
+    {
+        long sleepDurationInUs = targetDurationInUs - executionDurationInUs;
+        std::this_thread::sleep_for(std::chrono::microseconds(sleepDurationInUs));
+    }
+
+    AdjustInternals(executionDurationInUs);
+}
+
+uint32_t Metronome::UpdatesPerSecond()
+{
+    return _updatesPerSecond;
+}
+
+long Metronome::TargetDurationInUs()
+{
+    return std::lroundf(MICROSECONDS_PER_SECOND / _updatesPerSecond);
+}
+
+void Metronome::AdjustInternals(uint32_t executionDurationInUs)
+{
+    // Add new sample value and then remove the oldest value if the
+    // maximum number of samples has been exceeded
+    _historicExecutionDurationsInUs.push_back(executionDurationInUs);
+
+    if (_historicExecutionDurationsInUs.size() > MAX_HISTORIC_SAMPLES)
+    {
+        auto oldestValueIterator = _historicExecutionDurationsInUs.begin();
+        _executionDurationInUsAccumulator -= *oldestValueIterator;
+        _historicExecutionDurationsInUs.erase(oldestValueIterator);
+    }
+
+    // Add the new sample value to the accumulator itself
+    assert(_executionDurationInUsAccumulator <= UINT32_MAX - executionDurationInUs);
+    _executionDurationInUsAccumulator += executionDurationInUs;
+
+    // Nudge the number of updates per second (up or down) based on the average
+    if (_historicExecutionDurationsInUs.size() == MAX_HISTORIC_SAMPLES)
+    {
+        double averageExecDurationInUs =
+            (double)_executionDurationInUsAccumulator / MAX_HISTORIC_SAMPLES;
+
+        long averageUpdatesPerSecond = lround(MICROSECONDS_PER_SECOND / averageExecDurationInUs);
+
+        if (averageUpdatesPerSecond > _updatesPerSecond)
+        {
+            _updatesPerSecond = std::min(_updatesPerSecond + 1, _maxUpdatesPerSecond);
+        }
+        else if (averageUpdatesPerSecond < _updatesPerSecond)
+        {
+            _updatesPerSecond = std::max(_updatesPerSecond - 1, _minUpdatesPerSecond);
+        }
+    }
+}
+
+AutoMetronome::AutoMetronome(Metronome &m) : _metronome(m)
+{
+    _startTime = _metronome.StartMeasurement();
+}
+
+AutoMetronome::~AutoMetronome()
+{
+    _metronome.StopMeasurementAndSleepIfApplicable(_startTime);
+}
